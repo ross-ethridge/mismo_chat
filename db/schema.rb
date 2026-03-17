bb28ccc5f1eccb008d3daceb2490da04ead3d678
Ross Ethridge <ross.ethridge@gmail.com>
2026-03-17 17:10:44 -0500

    correcting the db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_28_180145) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_28_190000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
+    t.integer "conversation_id"
     t.datetime "created_at", null: false
     t.string "role"
     t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
+
+  add_foreign_key "messages", "conversations"
 end
