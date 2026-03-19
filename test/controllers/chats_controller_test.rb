f1e9e3f47c5cf6e2c31a9fa73363e76d63aba27c
Ross Ethridge <ross.ethridge@gmail.com>
2026-03-18 23:03:17 -0500

    adding controller for tests

--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class ChatsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get chats_index_url
+    get chats_url
     assert_response :success
   end
 
-  test "should get create" do
-    get chats_create_url
-    assert_response :success
+  test "should create chat" do
+    post chats_url
+    assert_response :redirect
   end
 end
